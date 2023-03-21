256dbc7c49863063590a9519d6aed6b86f58488d
your_father <Dav.id.bazhe.nov7@gmail.com>
2023-03-21 18:39:56 +0300

    newact

---
+++ app/src/main/java/com/example/adminapp/NewStatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.adminapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewStatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_stat)
+    }
+}
